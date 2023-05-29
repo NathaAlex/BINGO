55b6c920ccac269ce31527c06cba36e25f7defb5
Nathan Álex <104921446+NathaAlex@users.noreply.github.com>
2023-05-29 08:48:49 -0300

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+function play() {
+    var nome = prompt('Digite o nome que terá na cartela!');
+
+
+    insertCard();
+}
+
+function insertCard() {
+    var cartela = document.getElementById("tabelas")
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell2 = row.insertCell(1);
+    var cell2 = row.insertCell(1);
+}
